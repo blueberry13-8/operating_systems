34db99adc4983be96f3cc7ffe17441373e7d875c
blueberry13-8 <69643322+blueberry13-8@users.noreply.github.com>
2022-10-11 17:24:11 +0300

    Create ex1.c

---
+++ week06/ex1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    double att = 0, awt = 0;
+    int* at = malloc(sizeof(int)*n);
+    int* bt = malloc(sizeof(int)*n);
+    for (int i = 0; i < n; i++){
+        scanf("%d %d", at+i, bt+i);
+    }
+    int t = 0, executed = 0;
+    while (executed < n){
+        int j = 0;
+        for (int i = 0; i < n; i++){
+            if (bt[j] == -10 || bt[i] != -10 && at[i] <= t && at[i] < at[j]){
+                j = i;
+            }
+        }
+        if (at[j] > t || bt[j] == -10){
+            t++;
+            continue;
+        }
+        executed++;
+        awt += t - at[j];
+        att += bt[j] + t - at[j];
+        printf("for %d-th process CT: %d, TAT: %d, WT: %d\n", j, t + bt[j], bt[j] + t - at[j], t - at[j]);
+        t += bt[j];
+        bt[j] = -10;
+    }
+    printf("ATT: %f, AWT: %f\n", att/n, awt/n);
+    return 0;
+}
